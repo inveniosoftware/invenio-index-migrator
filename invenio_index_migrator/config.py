65392cbf3088df3993e2862a029df30f1ea3c38f
Georgios Lignos <glignos93@gmail.com>
2019-10-10 14:39:41 +0200

    remove older es pip package

--- invenio_index_migrator/config.py
+++ invenio_index_migrator/config.py
@@ -12,9 +12,46 @@ from __future__ import absolute_import, print_function
 
 # TODO: refactoring of the `strategy` field:
 # since it currently has two variations only and seems to be used only at one
-# specific place, it might be simplified in a boolean or extended for other
-# uses in the migration/jobs.
-INDEX_MIGRATOR_RECIPES = {}
+# specific place, it might be simplified in a boolean or extended for other uses in the migration/jobs.
+INDEX_MIGRATOR_RECIPES = dict(
+    deposits=dict(
+        cls='invenio_index_migrator.api.Migration',
+        params=dict(
+            strategy='cross_cluster_strategy',
+            src_es_client=dict(
+                prefix='',
+                version=2,
+                params=dict(
+                    host='zenodo-qa-search-c1.cern.ch',
+                    port=443,
+                    use_ssl=True,
+                    http_auth='zenodo:fjaX9b3V1YcrnXsoE2',
+                    url_prefix='',
+                    verify_certs=False
+                ),
+            ),
+            jobs=dict(
+                deposit_simple_reindex=dict(
+                    cls='invenio_index_migrator.api.ReindexAndSyncJob',
+                    pid_type='depid',
+                    index='deposits-records-record-v1.0.0',
+                    rollover_threshold=10,
+                    reindex_params=dict(
+                        source=dict(
+                            sort=dict(
+                                _created='desc'
+                            )
+                        ),
+                        dest=dict(
+                            op_type='create'
+                        ),
+                    ),
+                ),
+            )
+        )
+    )
+)
+
 """Index sync job definitions.
 
 Example:
